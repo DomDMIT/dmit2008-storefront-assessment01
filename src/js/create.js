f8ecef04affaeff2006ceef6e525c4fc7bf6096c
Dominick Drahokoupil <dom.drahokoupil@outlook.com>
2022-02-12 18:27:40 -0700

    Added images and progress on javascript and html

--- src/js/create.js
+++ src/js/create.js
@@ -17,5 +17,34 @@ function onImageSelected(e) {
 }
 
 async function uploadNewGame() {
-    
+    const cover = document.querySelector('#gameCover').files[0];
+    const title = document.querySelector('#gameTitle').value.trim();
+    const price = document.querySelector('#gamePrice').value.trim();
+    const genre = document.querySelector('#gameGenre').value.trim();
+    const rating = document.querySelector('#ageRating').value.trim();
+    const release = document.querySelector('#releaseDate').value.trim();
+
+    const imageRef = storageRef( storage, `images/${cover.name}`);
+    const dataRef = databaseRef( db, 'games');
+
+    const uploadResult = await uploadBytes(imageRef, file);
+
+    const urlPath = await getDownloadURL(imageRef);
+
+    const storagePath = uploadResult.metadata.fullPath;
+
+    const itemRef = await push(dataRef);
+
+    set(itemRef,{
+        key: itemRef.key,
+        sku:`dmit${itemRef.key}`,
+        urlPath,
+        storagePath,
+        title,
+        price,
+        genre,
+        rating,
+        release
+    })
+
 }
--- src/js/templates/productCard.js
+++ src/js/templates/productCard.js
@@ -1,5 +1,26 @@
-function productCard({key, gameTitle, urlPath}) {
-    // Add code for our card layout
+function productCard({key, gameTitle, gamePrice, gameGenre, ageRating, releaseDate, urlPath}) {
+    const template = 
+    `
+    <div class="card" style="width: 18rem;">
+    <img src="${urlPath}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${gameTitle}</h5>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">Price: ${gamePrice}</li>
+      <li class="list-group-item">Genre: ${gameGenre}</li>
+      <li class="list-group-item">Rating: ${ageRating}</li>
+      <li class="list-group-item">Release: ${releaseDate}</li>
+    </ul>
+    <div class="card-body">
+        <button id="edit" data-key="${key}" >Edit</button>
+        <button id="delete" data-key="${key}" >Delete</button>
+    </div>
+  </div>
+    `
+    const element = document.createRange().createContextualFragment(template).children[0]
+    addCardControls(element)
+    return element
 }
 
 function addCardControls(game){
